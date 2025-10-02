168ed02a99df238b2383bc9d87796cdc92296e45
elammaryo <122314668+elammaryo@users.noreply.github.com>
2025-10-02 14:02:43 -0400

    portfolio setup

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { Sun, Moon } from "lucide-react";
+
+export const ThemeToggle = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  return (
+    <button
+      onClick={() => {
+        if (isDarkMode) {
+          document.documentElement.classList.add("dark");
+        } else {
+          document.documentElement.classList.remove("dark");
+        }
+        setIsDarkMode(!isDarkMode);
+      }}
+    >
+      {isDarkMode ? (
+        <Sun className="h-6 w-6 text-yellow-300" />
+      ) : (
+        <Moon className="h-6 w-6 text-blue-900" />
+      )}
+    </button>
+  );
+};
---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+
+export const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground">
+      {/* Theme Toggle */}
+      <ThemeToggle />
+      {/* Background Effects */}
+
+      {/* NavBar */}
+
+      {/* Main Content */}
+
+      {/* Footer */}
+    </div>
+  );
+};
