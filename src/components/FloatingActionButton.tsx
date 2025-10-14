9c88548efabec32fd185064ea4c1fd88669daf0d
elammaryo <122314668+elammaryo@users.noreply.github.com>
2025-10-13 21:35:53 -0400

    fix safari button bug + add fade in chat transiton

--- src/components/FloatingActionButton.tsx
+++ src/components/FloatingActionButton.tsx
@@ -33,28 +33,32 @@ export const FloatingActionButton = () => {
     <div className="fixed justify-center w-auto right-8 bottom-10 md:right-12 md:bottom-12">
       <div
         className={cn(
+          "fixed md:max-w-120 inset-x-8 md:inset:none bottom-26 md:bottom-28 z-100 md:translate-x-[calc(100vw-560px)]",
+          "transition-opacity transition-transform duration-700 ease-out",
           isChatOpen
-            ? "fixed md:max-w-120 inset-x-8 md:inset:none bottom-26 md:bottom-28 z-100 md:translate-x-[calc(100vw-560px)]"
-            : "hidden"
+            ? "opacity-100 translate-y-0 pointer-events-auto"
+            : "opacity-0 translate-y-4 pointer-events-none"
         )}
       >
         <GlazeBot setIsChatOpen={setIsChatOpen} />
       </div>
-      <button
-        id="chat-button"
-        aria-label="open chat"
-        onClick={() => {
-          setIsChatOpen(!isChatOpen);
-        }}
-        className="group inline-flex rounded-md px-4 py-4 items-center justify-center hover:shadow-lg duration-300 glass glass-tint !bg-transparent prism-premium cursor-auto"
-      >
-        <div className="flex z-10 group-hover:gap-2 group-[.reveal-active]:gap-2">
-          <BotMessageSquare />
-          <span className="reveal-label font-bold italic overflow-hidden whitespace-nowrap max-w-0 opacity-0 transition-[max-width, opacity] duration-800 ease-out group-hover:max-w-[140px] group-hover:opacity-100 group-focus-visible:max-w-[140px] group-focus-visible:opacity-100">
-            GlazeBot AI
-          </span>
-        </div>
-      </button>
+      <div className="rounded-3xl overflow-hidden glass glass-tint hover:shadow-lg">
+        <button
+          id="chat-button"
+          aria-label="open chat"
+          onClick={() => {
+            setIsChatOpen(!isChatOpen);
+          }}
+          className="group inline-flex rounded-md px-4 py-4 items-center justify-center duration-300 !bg-transparent prism-premium cursor-auto"
+        >
+          <div className="flex z-10 group-hover:gap-2 group-[.reveal-active]:gap-2">
+            <BotMessageSquare />
+            <span className="reveal-label font-bold italic overflow-hidden whitespace-nowrap max-w-0 opacity-0 transition-[max-width, opacity] duration-800 ease-out group-hover:max-w-[140px] group-hover:opacity-100 group-focus-visible:max-w-[140px] group-focus-visible:opacity-100">
+              GlazeBot AI
+            </span>
+          </div>
+        </button>
+      </div>
     </div>
   );
 };
