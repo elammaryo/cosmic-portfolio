9bea2eef421f1c92f3b45b505db74966121948fa
elammaryo <122314668+elammaryo@users.noreply.github.com>
2025-11-28 05:10:15 -0500

    update resume file name

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -32,7 +32,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/documents/resume.pdf"
+                href="/documents/Omer_Elammary_Resume.pdf"
                 download="Omer_Elammary_Resume.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
