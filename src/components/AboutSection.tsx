7cdce803d75e21df970b34f886da39166b3bade5
elammaryo <122314668+elammaryo@users.noreply.github.com>
2025-11-28 05:06:46 -0500

    move resume to public

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -32,7 +32,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="src/assets/documents/resume.pdf"
+                href="/documents/resume.pdf"
                 download="Omer_Elammary_Resume.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
